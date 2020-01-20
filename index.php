df102dd12586297bb058999979082897b83c73aa
root <root@ip-172-31-32-238.eu-north-1.compute.internal>
2020-01-20 15:07:31 +0000

    change text1

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ else
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application - AWS Elastic Beanstalk</title>
+    <title>PHP Application - AWS Elastic Beanstalk! </title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
